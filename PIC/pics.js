daeee56d9f661c02fa9db203765cd61f0f2de513
krisgh <kris.mdw@gmail.com>
2013-03-13 12:19:42 +0800

    Optimize effective callback mode, IE compatible, button function complete.

--- PIC/pics.js
+++ PIC/pics.js
@@ -55,45 +55,80 @@ PICS.prototype.initialize=function(xml_path,width,height){
 PICS.prototype.fill_image=function(index_img,contaner){
 	var org_img=new Image();
 	var fill_img=new Image();
-	var index=index_img;
+	var index=parseInt(index_img);
 	var pics=this;
-	
+	var img_t_cache,img_s_cache;
+	var objcont=document.getElementById(contaner);
+	objcont.style.zoom=1;
 	org_img.onload=function (){
 		var ua=navigator.userAgent
 		if(ua.match(/(IE|MSIE)/i)){
 			if(org_img.readyState){
 				fill_img.src=org_img.src;
 				pics._workspace.style.background="url("+org_img.src+") no-repeat center center";
-				document.getElementById(contaner).appendChild(pics._workspace);
+				objcont.appendChild(pics._workspace);
 			}
 		}
 		else{
 			if(org_img.complete){
 				fill_img.src=org_img.src;
 				pics._workspace.style.background="url("+org_img.src+") no-repeat center center";
-				document.getElementById(contaner).appendChild(pics._workspace);
+				objcont.appendChild(pics._workspace);
 				}
 		}
 	}
-	
-	org_img.title=this.pic_que[index].t;
-	org_img.src=this.pic_que[index].s;
+
+	img_t_cache=this.pic_que[index].t||"";
+	img_s_cache=this.pic_que[index].s||"";
+	_fadeout_(objcont,effect_chain,this);
+	function effect_chain(fade_obj,callback_obj){
+		org_img.title=img_t_cache;
+		org_img.src=img_s_cache;
+		_fadein_(fade_obj);
+	}
 };
 
 //button bar
-PICS.prototype.btn_bar=function(){
-	var objbar,i;
+PICS.prototype.btn_bar=function(Pics){
+	var objbar,i,objli,obja;
 	objbar=document.createElement("div");
-	for(i=0;i<this.pic_que.length;i++){
-		objbar.addElement("<a id='nav"+i+"'>"+i+"</a>");
+	objbar.id="bs";
+	objbar.innerHTML="<ul>"+objbar.innerHTML+"</ul>"
+	for(var i=0;i<this.pic_que.length;i++){
+		objli=document.createElement("li");
+		obja=document.createElement("a");
+		obja.id="num"+i;
+		obja.href="javascript:void(0)";
+		obja.innerHTML=i;
+		obja.onclick=function(event){
+			if(this.className!=="selected"){
+				var arryclass=getElementsByClassName(objbar,"a","selected")
+				if(arryclass.length){
+					arryclass[0].className="";
+				}
+				this.className="selected";
+				Pics.fill_image(this.innerText,"show");
+			}
+		};
+		objli.appendChild(obja);
+		objbar.firstChild.appendChild(objli);
 	}
 	objbar.style.right="10px";
 	objbar.style.bottom="5px";
+	objbar.style.backgroundColor="yellow";
+	objbar.style.opacity=0.5;
+	objbar.onmouseover=function(){
+		objbar.style.opacity=1;
+	};
+	objbar.onmouseout=function(){
+		objbar.style.opacity=0.5;
+	};
 	return objbar;
 };
+
 //effective
-PICS.prototype._fadeout_=function(movobj){
-	var opacity_offset,speed,fading;
+function _fadeout_(movobj,callback,callback_obj){
+	var opacity_offset,speed;
 	speed=6;
 	function decrease(){
 		if(movobj.style.opacity===""){
@@ -102,49 +137,66 @@ PICS.prototype._fadeout_=function(movobj){
 		else{
 			opacity_offset=parseFloat(movobj.style.opacity,10);
 		}
-		fading=true;
+
 		if(opacity_offset>0.02){
 			movobj.style.opacity=opacity_offset-0.02;
+			movobj.style.filter="alpha(opacity="+(opacity_offset-0.02)*100+")";
 			setTimeout(function(){return decrease();},speed);
 		}
 		else{
 			movobj.style.opacity=0;
-			fading=false;
+			movobj.style.filter="alpha(opacity=0)";
+			if(typeof(callback)==="function"){
+				callback(movobj,callback_obj);
+			}
 		}
 	}
 
-	function end(){
-		return fading;
-	}
-	
 	decrease();
 };
 
-PICS.prototype._fadein_=function(movobj){
-	var opacity_offset,speed,fading;
+function _fadein_(movobj,callback,callback_obj){
+	var opacity_offset,speed;
 	speed=6;
 	function increase(){
 		if(movobj.style.opacity===""){
-			return fading
+			return this.fading
 		}
 		else{
 			opacity_offset=parseFloat(movobj.style.opacity,10);
 		}
-		fading=true;
+
 		if(opacity_offset<1){
 			movobj.style.opacity=opacity_offset+0.02;
+			movobj.style.filter="alpha(opacity="+(opacity_offset+0.02)*100+")";
 			setTimeout(function(){return increase();},speed);
 		}
 		else{
 			movobj.style.opacity=1;
-			fading=false;
+			movobj.style.filter="alpha(opacity=100)";
+			if(typeof(callback)==="function"){
+				callback(movobj,callback_obj);
+			}
 		}
 	}
 
-	function end(){
-		return fading;
-	}
-	
 	increase();
+};
+
+//getElementsByClassName
+//Author:Robert Nyman
+function getElementsByClassName(oElm, strTagName, strClassName){
+	var arrElements = (strTagName == "*" && oElm.all)?oElm.all:oElm.getElementsByTagName(strTagName);
+    var arrReturnElements = new Array();
+    strClassName = strClassName.replace(/\-/g, "\\-");
+    var oRegExp = new RegExp("(^|\\s)" + strClassName + "(\\s|$)");
+    var oElement;
+    for(var i=0; i < arrElements.length; i++){
+        oElement = arrElements[i];
+        if(oRegExp.test(oElement.className)){
+            arrReturnElements.push(oElement);
+        }
+    }
+    return (arrReturnElements)
 };
 })(this)
